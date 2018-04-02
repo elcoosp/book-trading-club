20ef0ae8c55d374469c22a5a55fdee9f8453c871
Elcoosp <elcoosp@gmail.com>
2018-04-02 16:42:30 +0200

    Add more deps (helmet, sanitizer)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-const mongoose = require('mongoose'),
+// Global packages
+const express = require('express'),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  mongoSanitize = require('express-mongo-sanitize'),
+  helmet = require('helmet'),
+  expressSanitizer = require('express-sanitizer'),
   dotenv = require('dotenv').config(),
   to = require('await-to-js').to,
+  // App modules and constants
   { MONGODB_URI } = process.env,
   User = require('./models/User'),
-  Book = require('./models/Book')
+  Book = require('./models/Book'),
+  app = express()
 
+// App initialization
+app
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(bodyParser.json())
+  .use(mongoSanitize())
+  .use(expressSanitizer())
+  .use(helmet())
+
+//Db connection
 mongoose
   .connect(MONGODB_URI)
   .then(() => console.log('coonnected to db'))
