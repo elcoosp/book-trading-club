c38bd139538ad3f4c61c4884d6bed4593770e155
Elcoosp <elcoosp@gmail.com>
2018-04-02 22:40:02 +0200

    Prefix routes by 'api"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 // Global packages
 const express = require('express'),
+  dotenv = require('dotenv').config(),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
   mongoSanitize = require('express-mongo-sanitize'),
   helmet = require('helmet'),
+  morgan = require('morgan'),
   expressSanitizer = require('express-sanitizer'),
-  dotenv = require('dotenv').config(),
   to = require('await-to-js').to,
   // App modules and constants
   { MONGODB_URI, PORT } = process.env,
-  userRoutes = require('./routes/user'),
-  bookRoutes = require('./routes/book'),
-  requestRoutes = require('./routes/request'),
+  userRoutes = require('./routes/users'),
+  bookRoutes = require('./routes/books'),
+  requestRoutes = require('./routes/requests'),
   app = express()
 
 // Db connection
@@ -25,11 +26,12 @@ app
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
   .use(mongoSanitize())
+  .use(morgan('tiny'))
   .use(expressSanitizer())
   .use(helmet())
   // Routes
-  .use('api/books', bookRoutes)
-  .use('api/users', userRoutes)
-  .use('api/requests', requestRoutes)
+  .use('/api/books', bookRoutes)
+  .use('/api/users', userRoutes)
+  .use('/api/requests', requestRoutes)
   // Listen
   .listen(PORT, () => console.log(`Server running on port ${PORT}`))
