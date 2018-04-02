80a26e1e4df3c1d448bae65ea5be99529bff0651
Elcoosp <elcoosp@gmail.com>
2018-04-02 22:41:13 +0200

    Dummy controller books + add private path for some schema fields when converted to JSON

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,19 +1,60 @@
-const to = require('await-to-js').to
+const to = require('await-to-js').to,
+  Book = require('../models/Book')
 
 module.exports = {
-  getAll(req, res) {
-    res.json({ msg: 'Not implemented yet' })
+  async getAll(req, res) {
+    const [e, books] = await to(Book.find())
+    e
+      ? res.status(404).json({
+          error: 'No books found'
+        })
+      : res.json(books)
   },
-  getOne(req, res) {
-    res.json({ msg: 'Not implemented yet' })
+
+  async getOne(req, res) {
+    const { params: { id } } = req
+    const [e, book] = await to(Book.findById(id))
+    e
+      ? res.status(404).json({
+          error: 'No book found'
+        })
+      : res.json(book)
   },
-  addOne(req, res) {
-    res.json({ msg: 'Not implemented yet' })
+
+  async addOne(req, res) {
+    const { body } = req
+
+    const [e, savedBook] = await to(new Book(body).save())
+
+    e
+      ? res.status(404).json({
+          error: 'Book not created'
+        })
+      : res.json(savedBook)
   },
-  updateOne(req, res) {
-    res.json({ msg: 'Not implemented yet' })
+
+  async updateOne(req, res) {
+    const { params: { id } } = req
+
+    const [e, updatedBook] = await to(Book.findByIdAndUpdate(id))
+    console.log(e)
+
+    e
+      ? res.status(404).json({
+          error: 'Book not updated'
+        })
+      : res.json(updatedBook)
   },
-  deleteOne(req, res) {
-    res.json({ msg: 'Not implemented yet' })
+
+  async deleteOne(req, res) {
+    const { params: { id } } = req
+
+    const [e, deleteResult] = await to(Book.findById(id).remove())
+
+    e || deleteResult.n === 0
+      ? res.status(404).json({
+          error: 'Book not deleted'
+        })
+      : res.status(200)
   }
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose'),
+  privatePaths = require('mongoose-private-paths'),
   { Schema } = mongoose
 
 const UserSchema = new Schema({
   nameFirst: { type: String, required: true },
   nameLast: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, private: true },
   books: [{ type: Schema.Types.ObjectId, ref: 'Book' }]
 })
 
