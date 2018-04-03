2cda23571eec72a50e36bc7dcc5422e2ac1fccec
Elcoosp <elcoosp@gmail.com>
2018-04-03 15:51:45 +0200

    Cerebral boilerplate

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Controller from './controller'
+import { Container } from '@cerebral/react'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Container controller={controller}>
+    <App />
+  </Container>,
+  document.getElementById('root')
+)
 registerServiceWorker()
