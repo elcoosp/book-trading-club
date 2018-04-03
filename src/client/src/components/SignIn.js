a953df7c3ebdd887b78c396461abeb57c50ee554
Elcoosp <elcoosp@gmail.com>
2018-04-03 18:36:02 +0200

    Dummy SignIn component

--- src/client/src/components/SignIn.js
+++ src/client/src/components/SignIn.js
@@ -1,3 +1,84 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Form, Icon, Input, Button, Checkbox } from 'antd'
+const FormItem = Form.Item
 
-export default () => <div>Signin</div>
+class SignInForm extends React.Component {
+  state = {
+    isLogin: true
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Signin form values: ', values)
+      }
+    })
+  }
+
+  toggleForm = () =>
+    this.setState(prevState => ({
+      isLogin: prevState.isLogin ? false : true
+    }))
+
+  render() {
+    const { getFieldDecorator } = this.props.form
+    const { isLogin } = this.state
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        {!isLogin && (
+          <Fragment>
+            <FormItem>
+              {getFieldDecorator('nameFirst', {
+                rules: [
+                  { required: true, message: 'Please enter your first name' }
+                ]
+              })(<Input placeholder="First name" />)}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('nameLast', {
+                rules: [
+                  { required: true, message: 'Please enter your last name' }
+                ]
+              })(<Input placeholder="Last name" />)}
+            </FormItem>
+          </Fragment>
+        )}
+
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please enter your username' }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please enter your Password' }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </FormItem>
+
+        <FormItem>
+          <Button type="primary" htmlType="submit">
+            Log in
+          </Button>
+        </FormItem>
+        <Button type="secondary" onClick={this.toggleForm}>
+          Or {isLogin ? 'register now' : 'login'}
+        </Button>
+      </Form>
+    )
+  }
+}
+
+const SignIn = Form.create()(SignInForm)
+
+export default SignIn
