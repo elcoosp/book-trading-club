798ee446bd4603f596f147b69baae72fdded53c1
Elcoosp <elcoosp@gmail.com>
2018-04-04 18:06:43 +0200

    Add register capability

--- src/client/src/components/SignIn.js
+++ src/client/src/components/SignIn.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react'
 import { connect } from '@cerebral/react'
 import { signal } from 'cerebral/tags'
-
+import styled from 'styled-components'
 import { Form, Icon, Input, Button, Checkbox } from 'antd'
 const FormItem = Form.Item
-
+const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 class SignInForm extends React.Component {
   state = {
     isLogin: true
@@ -15,8 +19,8 @@ class SignInForm extends React.Component {
     form.validateFields((err, formData) => {
       if (!err) {
         this.state.isLogin
-          ? _login({ loginForm: formData })
-          : _register({ registerForm: formData })
+          ? _login({ data: formData })
+          : _register({ data: formData })
       }
     })
   }
@@ -31,57 +35,63 @@ class SignInForm extends React.Component {
     const { isLogin } = this.state
 
     return (
-      <Form onSubmit={this.handleSubmit}>
-        {!isLogin && (
-          <Fragment>
-            <FormItem>
-              {getFieldDecorator('nameFirst', {
-                rules: [
-                  { required: true, message: 'Please enter your first name' }
-                ]
-              })(<Input placeholder="First name" />)}
-            </FormItem>
-            <FormItem>
-              {getFieldDecorator('nameLast', {
-                rules: [
-                  { required: true, message: 'Please enter your last name' }
-                ]
-              })(<Input placeholder="Last name" />)}
-            </FormItem>
-          </Fragment>
-        )}
-
-        <FormItem>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please enter your username' }]
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
-            />
+      <FormWrapper>
+        <Form onSubmit={this.handleSubmit}>
+          {!isLogin && (
+            <Fragment>
+              <FormItem>
+                {getFieldDecorator('nameFirst', {
+                  rules: [
+                    { required: true, message: 'Please enter your first name' }
+                  ]
+                })(<Input placeholder="First name" />)}
+              </FormItem>
+              <FormItem>
+                {getFieldDecorator('nameLast', {
+                  rules: [
+                    { required: true, message: 'Please enter your last name' }
+                  ]
+                })(<Input placeholder="Last name" />)}
+              </FormItem>
+            </Fragment>
           )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please enter your Password' }]
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
-            />
-          )}
-        </FormItem>
 
-        <FormItem>
-          <Button type="primary" htmlType="submit">
-            Log in
+          <FormItem>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: 'Please enter your username' }]
+            })(
+              <Input
+                prefix={
+                  <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                placeholder="Username"
+              />
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please enter your Password' }]
+            })(
+              <Input
+                prefix={
+                  <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                type="password"
+                placeholder="Password"
+              />
+            )}
+          </FormItem>
+
+          <FormItem>
+            <Button type="primary" htmlType="submit">
+              Log in
+            </Button>
+          </FormItem>
+          <Button type="secondary" onClick={this.toggleForm}>
+            Or {isLogin ? 'register now' : 'login'}
           </Button>
-        </FormItem>
-        <Button type="secondary" onClick={this.toggleForm}>
-          Or {isLogin ? 'register now' : 'login'}
-        </Button>
-      </Form>
+        </Form>
+      </FormWrapper>
     )
   }
 }
