a2be028b0ae9d2b0ac4f6abf567a74a287d8e018
Elcoosp <elcoosp@gmail.com>
2018-04-06 19:52:19 +0200

    Create an auth context shared across the app to replace cerebraljs

---
+++ src/client/src/context/Auth/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+const { Provider, Consumer } = React.createContext()
+
+export class AuthProvider extends Component {
+  state = {
+    isLoggedIn: false,
+    user: null
+  }
+  render() {
+    const { state, props: { children } } = this
+    return (
+      <Provider
+        value={{
+          S: state,
+          A: {
+            login: () =>
+              !state.isLoggedIn &&
+              this.setState(s => ({
+                isLoggedIn: true,
+                user: { name: 'SupaDupaUser' }
+              })),
+            logout: () =>
+              state.isLoggedIn &&
+              this.setState(s => ({
+                isLoggedIn: false,
+                user: null
+              }))
+          }
+        }}
+      >
+        {children}
+      </Provider>
+    )
+  }
+}
+
+export const withAuth = Component => props => (
+  <AuthConsumer>{auth => <Component {...props} auth={auth} />}</AuthConsumer>
+)
+
+export const AuthConsumer = Consumer
