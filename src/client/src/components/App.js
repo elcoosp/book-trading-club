a3cf1d3cf0da730b5583e18420c756e2b5ba80fe
Elcoosp <elcoosp@gmail.com>
2018-04-06 19:52:51 +0200

    Refactor routes from app to use context

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,6 +1,4 @@
 import React, { Component, Fragment } from 'react'
-import { connect } from '@cerebral/react'
-import { state, signal } from 'cerebral/tags'
 import { Route, withRouter } from 'react-router'
 import Home from './Home'
 import MyBooks from './MyBooks'
@@ -8,38 +6,35 @@ import Requests from './Requests'
 import Navigation from './Navigation'
 import Settings from './Settings'
 import SignIn from './SignIn'
+import { AuthProvider, withAuth } from '../context/Auth'
+
+const AppRoutes = withRouter(
+  withAuth(({ auth: { S: isLoggedIn } }) => (
+    <Fragment>
+      <Route exact path="/" component={Home} />
+      {isLoggedIn && (
+        <Fragment>
+          <Route path="/settings" component={Settings} />
+          <Route path="/mybooks" component={MyBooks} />
+          <Route path="/requests" component={Requests} />
+        </Fragment>
+      )}
+      <Route path="/signin" component={SignIn} />
+    </Fragment>
+  ))
+)
 
 class App extends Component {
-  componentDidMount = () => {
-    this.props._initialize()
-  }
-
   render() {
-    const { isAuthenticated } = this.props
-
     return (
       <div>
-        <Navigation />
-        <Route exact path="/" component={Home} />
-        {isAuthenticated && (
-          <Fragment>
-            <Route path="/settings" component={Settings} />
-            <Route path="/mybooks" component={MyBooks} />
-            <Route path="/requests" component={Requests} />
-          </Fragment>
-        )}
-        <Route path="/signin" component={SignIn} />
+        <AuthProvider>
+          <Navigation />
+          <AppRoutes />
+        </AuthProvider>
       </div>
     )
   }
 }
 
-export default withRouter(
-  connect(
-    {
-      _initialize: signal`initialize`,
-      isAuthenticated: state`auth.isAuthenticated`
-    },
-    App
-  )
-)
+export default App
