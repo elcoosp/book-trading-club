cb26a6c060d14f2a1f5b4b7957f7722aeaf21de9
Elcoosp <elcoosp@gmail.com>
2018-04-18 00:04:30 +0200

    add: context with decode payload if any

--- server/index.js
+++ server/index.js
@@ -2,16 +2,30 @@ const { GraphQLServer } = require('graphql-yoga')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const resolvers = require('./resolvers')
+const to = require('await-to-js').to
+const jwt = require('jsonwebtoken')
+
+const jwtVerify = (token, secret) =>
+  jwt.verify(
+    token,
+    secret,
+    (e, payload) =>
+      new Promise((resolve, reject) => (e ? reject(e) : resolve(payload)))
+  )
 dotenv.config()
-const { MONGODB_URI, PORT } = process.env
+const { MONGODB_URI, PORT, JWT_SECRET } = process.env
 
 mongoose.connect(MONGODB_URI).then(() => console.log('CONNECTED TO DB'))
 
 const server = new GraphQLServer({
   typeDefs: './server/schema.graphql',
   resolvers,
-  context: {
-    db: mongoose.connection
+  context: async ({ request: { headers } }) => {
+    if (headers.authorization) {
+      const token = headers.authorization.split('Bearer ')[1]
+      const [e, user] = await to(jwtVerify(token, JWT_SECRET))
+      return { user }
+    }
   }
 })
 server.start(
