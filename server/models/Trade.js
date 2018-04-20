4713203ca49adb23ef0bc8c3b3ae68f0448c16c7
Elcoosp <elcoosp@gmail.com>
2018-04-20 23:12:35 +0200

    rename: bookId to book on Trade model/schema

--- server/models/Trade.js
+++ server/models/Trade.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Trade = new Schema({
-  bookId: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+  book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
   requester: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   accepted: { type: Boolean, required: true, default: false }
 })
