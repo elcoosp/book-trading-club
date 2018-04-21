30e4b601ec5974ff238cdb5c0234f938cee7cf3f
Elcoosp <elcoosp@gmail.com>
2018-04-21 22:51:11 +0200

    add: theme file

---
+++ client/src/theme.js
@@ -0,0 +1,24 @@
+const colors = {
+  dark: 'rgba(52, 52, 74, 1)',
+  purple: ' rgba(128, 71, 94, 1)',
+  red: 'rgba(204, 90, 113, 1)',
+  brown: 'rgba(200, 155, 123, 1)',
+  yellow: 'rgba(240, 247, 87, 1)'
+}
+const spacing = {
+  small: '5px',
+  medium: '10px',
+  large: '20px'
+}
+
+const shadow = {
+  small: '0 2px 3px 1px rgba(0, 0, 0, 0.2);',
+  medium: '0 2px 6px 1px rgba(0, 0, 0, 0.2);',
+  large: '0 2px 8px 1px rgba(0, 0, 0, 0.2);'
+}
+const theme = {
+  colors,
+  spacing,
+  shadow
+}
+export default theme
