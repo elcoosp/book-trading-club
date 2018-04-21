9ef353c490b0135febfaf565b28aa8d46a9dcce0
Elcoosp <elcoosp@gmail.com>
2018-04-21 22:57:27 +0200

    add: nav comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Books from './components/Books'
 import Home from './components/Home'
 import NewBook from './components/NewBook'
 import Settings from './components/Settings'
 import Trades from './components/Trades'
+import Navigation from './components/Navigation'
+
 import { FragmentsOnCompositeTypes } from 'graphql'
 
 class App extends Component {
   render() {
     return (
       <Fragment>
+        <Navigation />
         <Route exact path="/" component={Home} />
         <Route path="/books" component={Books} />
         <Route path="/books/new" component={NewBook} />
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+const Nav = styled.div`
+  background-color: ${p => p.theme.colors.brown};
+  color: white;
+`
+export default () => {
+  return (
+    <Nav>
+      <Link to="/">Home</Link>
+      <Link to="/books">Books</Link>
+      <Link to="/settings">Settings</Link>
+      <Link to="/trades">Trades</Link>
+    </Nav>
+  )
+}
