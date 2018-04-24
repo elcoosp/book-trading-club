d49740f9704929c60a03cb1f99a76f1c4e586ad3
Elcoosp <elcoosp@gmail.com>
2018-04-24 22:51:21 +0200

    add: basic form for settings with mutation

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,19 +1,108 @@
 import { gql } from 'apollo-boost'
-import { Query } from 'react-apollo'
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import { notEmpty } from '../utils'
 
-const GET_BOOKS = gql`
-  query books {
-    books {
-      _id
-      title
-      author
+const withMutation = comp =>
+  graphql(UPDATE_USER, { name: 'updateUserMutation' })(comp)
+const UPDATE_USER = gql`
+  mutation updateUser(
+    $pseudo: String
+    $password: String
+    $fullName: String
+    $city: String
+    $state: String
+  ) {
+    updateUser(
+      pseudo: $pseudo
+      password: $password
+      fullName: $fullName
+      city: $city
+      state: $state
+    ) {
+      pseudo
+      fullName
+      city
+      state
     }
   }
 `
 
-export default class Settings extends Component {
-  render() {
-    return <div>settings</div>
+export default withMutation(
+  class Settings extends Component {
+    state = {
+      pseudo: '',
+      password: '',
+      fullName: '',
+      city: '',
+      state: ''
+    }
+
+    onChange = ({ target: { value, name } }) =>
+      this.setState(prevState => ({
+        [name]: value
+      }))
+
+    onSubmit = e => {
+      e.preventDefault()
+      if (notEmpty(...Object.values(this.state))) {
+        this.props
+          .updateUserMutation({
+            variables: this.state
+          })
+          .then(console.log)
+          .catch(console.log)
+      }
+    }
+
+    render() {
+      const { pseudo, password, fullName, city, state } = this.state
+      return (
+        <main>
+          <form onSubmit={this.onSubmit}>
+            <label htmlFor="pseudo">Pseudo</label>
+            <input
+              name="pseudo"
+              type="text"
+              value={pseudo}
+              onChange={this.onChange}
+            />
+
+            <label htmlFor="password">New password</label>
+            <input
+              name="password"
+              type="password"
+              value={password}
+              onChange={this.onChange}
+            />
+
+            <label htmlFor="fullName">Full name</label>
+            <input
+              name="fullName"
+              type="text"
+              value={fullName}
+              onChange={this.onChange}
+            />
+
+            <label htmlFor="city">City</label>
+            <input
+              name="city"
+              type="text"
+              value={city}
+              onChange={this.onChange}
+            />
+
+            <label htmlFor="state">State</label>
+            <input
+              name="state"
+              type="text"
+              value={state}
+              onChange={this.onChange}
+            />
+            <button type="submit">Change settings</button>
+          </form>
+        </main>
+      )
+    }
   }
-}
+)
