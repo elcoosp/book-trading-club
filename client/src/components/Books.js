bc24658c8cf3bbe709a4536be2b18bf334dd30f0
Elcoosp <elcoosp@gmail.com>
2018-04-28 12:37:55 +0200

    add: request trade mutation
    Changed schema to accept String and not ID as args

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { gql } from 'apollo-boost'
 import QueryLoaderError from './QueryLoaderError'
+import { log } from 'util'
+import { Mutation } from 'react-apollo'
 
 const GET_BOOKS = gql`
   query {
@@ -13,7 +15,21 @@ const GET_BOOKS = gql`
   }
 `
 
+const REQUEST_TRADE = gql`
+  mutation requestTrade($bookId: String!) {
+    requestTrade(book: $bookId) {
+      _id
+    }
+  }
+`
+
 export default class Books extends Component {
+  state = {
+    requestTrade: {
+      error: null
+    }
+  }
+
   render() {
     return (
       <Fragment>
@@ -21,13 +37,48 @@ export default class Books extends Component {
         <QueryLoaderError
           query={GET_BOOKS}
           finalComp={({ books }) => (
-            <ul>
-              {books.map(({ title, author, _id }) => (
-                <li key={_id}>
-                  <h1>{title}</h1> <h2>{author}</h2>
-                </li>
-              ))}
-            </ul>
+            <Mutation mutation={REQUEST_TRADE}>
+              {(requestTrade, data) => (
+                <ul>
+                  {this.state.requestTrade.error && (
+                    <p>{this.state.requestTrade.error}</p>
+                  )}
+                  {this.state.requestTrade.success && (
+                    <p>{this.state.requestTrade.success}</p>
+                  )}
+
+                  {books.map(({ title, author, _id }) => (
+                    <li key={_id}>
+                      <h1>{title}</h1>
+                      <h2>{author}</h2>
+
+                      <button
+                        onClick={() =>
+                          requestTrade({ variables: { bookId: _id } })
+                            .then(data =>
+                              this.setState(prevState => ({
+                                requestTrade: {
+                                  success: `${title} was successfully requested`
+                                }
+                              }))
+                            )
+                            .catch(e =>
+                              this.setState(prevState => ({
+                                requestTrade: {
+                                  error:
+                                    'Could not request trade, an error  occured'
+                                }
+                              }))
+                            )
+                        }
+                      >
+                        Request Trade
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </Mutation>
           )}
         />
       </Fragment>
